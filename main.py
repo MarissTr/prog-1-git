c49716b4b56af9c0918e84f6900ffdb6c9faf46e
MarissTr <mariss.troinikovs@valmiera.edu.lv>
2024-09-19 15:06:52 +0300

    (mašīnas klase)

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Sveika pasaule")
+class Mašīnas:
+    def __init__(self,modelis,krasa,marka):
+        self.modelis = modelis
+        self.marka = marka
+        self.krasa = krasa
+    def est(self):
+        print("sveiki")
+
+m1= Mašīnas("BMW","E90","melns")
+m1.est()
